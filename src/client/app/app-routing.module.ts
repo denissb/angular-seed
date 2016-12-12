aa5f541ad165a83b8df924f0cfa180f4ffcee4d8
Deniss Borisovs <deniss.borisovs@gmail.com>
2016-12-12 21:37:23 +0100

    feat: loading route modules asynchronously

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -7,6 +7,9 @@ import { RouterModule } from '@angular/router';
       /* define app module routes here, e.g., to lazily load a module
          (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
        */
+       { path: '', loadChildren: 'app/home/home.module#HomeModule' },
+       { path: 'about', loadChildren: 'app/about/about.module#AboutModule' },
+       { path: 'home', loadChildren: 'app/home/home.module#HomeModule' }
     ])
   ],
   exports: [RouterModule]
--- tools/tasks/seed/build.bundles.app.ts
+++ tools/tasks/seed/build.bundles.app.ts
@@ -13,11 +13,87 @@ const BUNDLER_OPTIONS = {
  * Executes the build process, bundling the JavaScript files using the SystemJS builder.
  */
 export = (done: any) => {
-  let builder = new Builder(Config.SYSTEM_BUILDER_CONFIG);
+  let builder = new Builder({
+    defaultJSExtensions: true,
+    base: this.PROJECT_ROOT,
+    packageConfigPaths: [
+      join('node_modules', '*', 'package.json'),
+      join('node_modules', '@angular', '*', 'package.json')
+    ],
+    paths: {
+      // Note that for multiple apps this configuration need to be updated
+      // You will have to include entries for each individual application in
+      // `src/client`.
+      [join('dist/tmp', 'app', '*')]: 'dist/tmp/app/*',
+      'dist/tmp/node_modules/*': 'dist/tmp/node_modules/*',
+      'node_modules/*': 'node_modules/*',
+      '*': 'node_modules/*'
+    },
+    packages: {
+      '@angular/common': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/compiler': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/core/testing': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/core': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/forms': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/http': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/platform-browser': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/platform-browser-dynamic': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/router': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      'rxjs': {
+        main: 'Rx.js',
+        defaultExtension: 'js'
+      }
+    }
+  });
+  
+  // builder
+  //   .buildStatic(join(Config.TMP_DIR, Config.BOOTSTRAP_PROD_MODULE),
+  //     join(Config.JS_DEST, Config.JS_PROD_APP_BUNDLE),
+  //     BUNDLER_OPTIONS)
+  //   .then(() => done())
+  //   .catch((err: any) => done(err));
+
+  // Trying to split modules into multiple bundles
+  let dir = 'dist/prod';
+
   builder
-    .buildStatic(join(Config.TMP_DIR, Config.BOOTSTRAP_PROD_MODULE),
-      join(Config.JS_DEST, Config.JS_PROD_APP_BUNDLE),
-      BUNDLER_OPTIONS)
-    .then(() => done())
-    .catch((err: any) => done(err));
+  .buildStatic(
+    join('dist/tmp', 'app/main'),
+    join(`${dir}/js`, 'app.js'),
+    BUNDLER_OPTIONS)
+    .then(() => builder.bundle(
+      join('dist/tmp', 'app/about/index'),
+      join(`${dir}/js`, 'about/about.js')))
+    .then(() => builder.bundle(
+      join('dist/tmp', 'app/home/index'),
+      join(`${dir}/js`, 'home/home.js')))
+  .then(() => done())
+  .catch((err: any) => done(err));
 };
