f6d0b073b9d95fd6fa19897250abfa3222dfbdd5
Deniss Borisovs <deniss.borisovs@gmail.com>
2016-12-12 22:28:49 +0100

    Updated to latest version of angular

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, SystemJsNgModuleLoader } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ModuleLoaderComponent } from './loader/loader.component';
 
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [BrowserModule, HttpModule, AppRoutingModule, SharedModule.forRoot()],
-  declarations: [AppComponent],
-  providers: [{
+  declarations: [AppComponent, ModuleLoaderComponent],
+  providers: [
+  SystemJsNgModuleLoader,
+  {
     provide: APP_BASE_HREF,
     useValue: '<%= APP_BASE %>'
   }],
